db556c5c14b02fe72854dbcf3910ea13ccb6076a
Martin Míka <martin.mika@green-light.com>
2018-03-15 13:43:40 +0100

    Create index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './Decorators/Document';
+export * from './Decorators/Property';
+export * from './Decorators/OneToMany';
+export * from './Decorators/OneToOne';
+export * from './Repository';
+export * from './DocumentManager';
+export * from './BaseDocument';
--- test/connection.spec.ts
+++ test/connection.spec.ts
@@ -6,7 +6,7 @@ import { config } from './config/default';
 describe('Client', () => {
 
   it('should connect to server', async () => {
-    const db = await MongoClient.connect(config.odm.url);
+    const db = await MongoClient.connect((config.odm as any).url);
     expect(db.isConnected(config.odm.database)).to.equal(true);
     db.close();
   });
